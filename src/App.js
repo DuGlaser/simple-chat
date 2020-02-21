2c75648bf711e314dd25e510718670bd63167500
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2020-02-21 11:30:55 +0900

    :recycle:ADD title
    ~

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
-      <div className="App"></div>
+      <div className="App">Hello World</div>
     </BrowserRouter>
   );
 }
