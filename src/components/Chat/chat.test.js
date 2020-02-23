6e5e8097d00685add9df514c92925a7bb2748c66
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2020-02-24 00:29:38 +0900

    :rotating_light:FIX chat.test.js
    ~

--- src/components/Chat/chat.test.js
+++ src/components/Chat/chat.test.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { mount } from "enzyme";
-import Chat from "./chat";
+import ChatComponent from "./chat";
 import RoomHeader from "./roomHeader";
 import SendForm from "./sendForm";
+import MessageTile from "./messageTile";
 
 test("renders learn react link", () => {
   // shallow is not working
   // https://github.com/emotion-js/emotion/issues/1372
-  const wrapper = mount(<Chat />);
+  const wrapper = mount(
+    <ChatComponent loading={false} values={[{ message: 1 }, { message: 2 }]} />
+  );
   expect(wrapper.find(RoomHeader).length).toBe(1);
   expect(wrapper.find(SendForm).length).toBe(1);
+  expect(wrapper.find(MessageTile).length).toBe(2);
 });
