63ad657b5e4580c884886182cb874827af7a2653
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2020-02-23 12:37:38 +0900

    :sparkles:ADD SideMenu
    ~

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
+import Chat from "./components/Chat/chat";
+import styled from "@emotion/styled";
+import Side from "./components/SideMenu/side";
+
+const Flex = styled.div({
+  display: "flex"
+});
+
+const Div2 = styled.div({
+  width: "280px"
+});
+
+const Div8 = styled.div({
+  flexGrow: "1"
+});
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">Hello World</div>
+      <Flex>
+        <Div2>
+          <Side />
+        </Div2>
+        <Div8>
+          {/* TODO:ADD router */}
+          <Chat />
+        </Div8>
+      </Flex>
     </BrowserRouter>
   );
 }
