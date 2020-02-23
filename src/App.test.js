cb68450d1d5f4556c44b20d20120d3339f96d971
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2020-02-23 12:40:12 +0900

    :rotating_light:FIX App.test.js
    ~

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import React from "react";
 import App from "./App";
+import Side from "./components/SideMenu/side";
 import { shallow } from "enzyme";
 
 test("renders learn react link", () => {
   const wrapper = shallow(<App />);
-  expect(wrapper.find("div").length).toBe(1);
+  expect(wrapper.find(Side).length).toBe(1);
 });
