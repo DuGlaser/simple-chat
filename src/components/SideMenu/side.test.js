bd6eb73f9872273512fd0f9f9af199f7b51fde90
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2020-02-24 20:32:15 +0900

    :rotating_light:FIX test
    ~

--- src/components/SideMenu/side.test.js
+++ src/components/SideMenu/side.test.js
@@ -2,12 +2,12 @@ import React from "react";
 import Side from "./side";
 import AddFriendButton from "./addFriendButton";
 import SideListTitle from "./sideListTitle";
-import { mount } from "enzyme";
+import { mount, shallow } from "enzyme";
 
 test("renders learn react link", () => {
   // shallow is not working
   // https://github.com/emotion-js/emotion/issues/1372
-  const wrapper = mount(<Side />);
+  const wrapper = shallow(<Side />);
   expect(wrapper.find(AddFriendButton).length).toBe(1);
   expect(wrapper.find(SideListTitle).length).toBe(1);
 });
