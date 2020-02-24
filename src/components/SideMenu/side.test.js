0d095d05954cedd56c8999b7cd4769b949b6aca4
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2020-02-24 23:25:12 +0900

    :rotating_light:FIX side.test.js
    ~

--- src/components/SideMenu/side.test.js
+++ src/components/SideMenu/side.test.js
@@ -7,7 +7,9 @@ import { mount, shallow } from "enzyme";
 test("renders learn react link", () => {
   // shallow is not working
   // https://github.com/emotion-js/emotion/issues/1372
-  const wrapper = shallow(<Side />);
+  const wrapper = shallow(
+    <Side user={{ photoURL: "unknow", display: "hoge" }} />
+  );
   expect(wrapper.find(AddFriendButton).length).toBe(1);
   expect(wrapper.find(SideListTitle).length).toBe(1);
 });
