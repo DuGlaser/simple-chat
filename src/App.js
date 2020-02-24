39ace7d52431fbe091f1c49eca267c15955e3d2a
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2020-02-24 17:10:06 +0900

    :sparkles:ADD user info to firestore process
    ~

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
-import Chat from "./components/Chat/chat";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import Chat from "./components/Chat/index";
 import styled from "@emotion/styled";
 import Side from "./components/SideMenu/side";
+import { useAuthState } from "react-firebase-hooks/auth";
+import firebase from "firebase/app";
+import "firebase/auth";
+import { db } from "./config/firebaseConfig";
 
 const Flex = styled.div({
   display: "flex"
@@ -18,19 +22,82 @@ const Div8 = styled.div({
 });
 
 function App() {
+  const [user, initialising, error] = useAuthState(firebase.auth());
+
+  const login = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(snapshot => {
+        console.log(snapshot.additionalUserInfo.isNewUser);
+        if (snapshot.additionalUserInfo.isNewUser) {
+          db.collection("users")
+            .doc(snapshot.user.uid)
+            .set({
+              name: snapshot.user.displayName,
+              id: snapshot.user.uid,
+              avater: snapshot.user.photoURL
+            });
+        }
+      })
+      .catch(function(error) {
+        console.error("Error adding document: ", error);
+      });
+  };
+
+  if (initialising) {
+    return <div>loading...</div>;
+  }
+
+  if (error) {
+    return <div>error</div>;
+  }
+
   return (
     <BrowserRouter>
-      <Flex>
-        <Div2>
-          <Side />
-        </Div2>
-        <Div8>
-          {/* TODO:ADD router */}
-          <Chat />
-        </Div8>
-      </Flex>
+      {!user && (
+        <button
+          onClick={() => {
+            login();
+          }}
+        >
+          Google login
+        </button>
+      )}
+
+      {user && (
+        <Flex>
+          <Div2>
+            <Side />
+          </Div2>
+          <Div8>
+            {/* TODO:ADD router */}
+            <Route exact path="/" component={Home} />
+            <Route path="/chat" component={Chat} />
+          </Div8>
+        </Flex>
+      )}
     </BrowserRouter>
   );
 }
+
+const Home = () => {
+  const logout = () => {
+    firebase.auth().signOut();
+  };
+  return (
+    <div>
+      <Link to="/chat">Chat</Link>
+      <button
+        onClick={() => {
+          logout();
+        }}
+      >
+        Google logout
+      </button>
+    </div>
+  );
+};
 
 export default App;
