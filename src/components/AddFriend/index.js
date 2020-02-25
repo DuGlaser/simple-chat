c52be53824d2fe0d0237028618d186b4249ef935
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2020-02-25 17:02:15 +0900

    :sparkles:ADD index.js to components/AddFriend
    ~

---
+++ src/components/AddFriend/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import AddFriendContainer from "../../containers/AddFriend";
+
+const AddFriend = () => {
+  return (
+    <>
+      <AddFriendContainer />
+    </>
+  );
+};
+export default AddFriend;
