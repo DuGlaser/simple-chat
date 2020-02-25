e708236033784ac560e23c164a00e350c827ce1c
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2020-02-25 17:02:39 +0900

    :sparkles:ADD searchFrom.js
    ~

---
+++ src/components/AddFriend/searchFrom.js
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const Div = styled.div({
+  width: "80%",
+  margin: "0 auto",
+  display: "flex"
+});
+const Input = styled.input({
+  whiteSpace: "normal",
+  padding: "10px",
+  flexGrow: "1",
+  border: "1px solid #d4d4d4",
+  outline: "none",
+  lineHeight: "1rem",
+  "&:focus": { border: "1px solid #e53170", outline: "none" }
+});
+
+const SearchForm = props => {
+  console.log(props);
+  // TODO:low performance
+  return (
+    <Div>
+      <Input
+        onKeyUp={e => {
+          props.setUserName(e.target.value);
+        }}
+      />
+    </Div>
+  );
+};
+
+export default SearchForm;
