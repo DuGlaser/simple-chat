8f27ba29eff6a802a171a96679c756ffa2055017
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2020-02-25 23:04:21 +0900

    :sparkles:ADD Friend process
    ~

--- src/containers/AddFriend/index.js
+++ src/containers/AddFriend/index.js
@@ -1,18 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { v4 as uuidv4 } from "uuid";
 import AddFriendComponent from "../../components/AddFriend/addFriend";
 import { db } from "../../config/firebaseConfig";
+import { UserContext } from "../../context";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 
 const AddFriendContainer = () => {
   const [userName, setUserName] = useState("");
+  const myInfo = useContext(UserContext);
 
   const [users, loading, error] = useCollectionData(
     db.collection("users").where("name", "==", String(userName)),
     { idField: "id" }
   );
 
-  console.log(users);
+  const addFriendfunc = index => {
+    const roomId = uuidv4();
+    db.collection("users")
+      .doc(myInfo.uid)
+      .collection("friends")
+      .add({
+        avater: users[index].avater,
+        name: users[index].name,
+        id: users[index].id,
+        roomId: roomId
+      });
+    db.collection("users")
+      .doc(users[index].id)
+      .collection("friends")
+      .add({
+        avater: myInfo.photoURL,
+        name: myInfo.displayName,
+        id: myInfo.uid,
+        roomId: roomId
+      });
+    db.collection("rooms")
+      .doc(roomId)
+      .set({});
+  };
 
+  console.log(users);
   return (
     <>
       <AddFriendComponent
@@ -20,6 +47,7 @@ const AddFriendContainer = () => {
         users={users}
         loading={loading}
         error={error}
+        addFriend={addFriendfunc}
       />
     </>
   );
