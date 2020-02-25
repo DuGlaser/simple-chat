a99d5c92589c290091f37f2a0dad2c673e78a54f
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2020-02-25 13:30:01 +0900

    :sparkles:ADD friendsList.js
    ~

---
+++ src/components/SideMenu/friendsList.js
@@ -0,0 +1,39 @@
+import React from "react";
+import FriendCard from "../common/Card/FriendCard";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { db } from "../../config/firebaseConfig";
+
+const FriendsList = props => {
+  const [friends, loading, error] = useCollectionData(
+    db
+      .collection("users")
+      .doc(props.user.uid)
+      .collection("friends"),
+    { idField: "id" }
+  );
+
+  if (friends) {
+    return (
+      <>
+        {friends.map(friend => {
+          return (
+            <FriendCard
+              width="200px"
+              height="40px"
+              key={friend.id}
+              name={friend.name}
+              src={friend.avater}
+              roomId={friend.roomId}
+            />
+          );
+        })}
+      </>
+    );
+  } else if (error) {
+    return <div>error</div>;
+  } else {
+    return <div>loading...</div>;
+  }
+};
+
+export default FriendsList;
