2eb8a33587abfea3b716e65f1bc4f297f6f29496
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2020-02-25 13:22:33 +0900

    :sparkles:ADD send message process
    ~

--- src/components/Chat/chat.js
+++ src/components/Chat/chat.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import RoomHeader from "./roomHeader";
 import styled from "@emotion/styled";
 import MessageTile from "./messageTile";
 import SendForm from "./sendForm";
+import { db } from "../../config/firebaseConfig";
+import { UserContext } from "../../context";
 
 const Div = styled.div({
   width: "100%",
@@ -13,24 +15,42 @@ const Div = styled.div({
 
 const ChatArea = styled.div({
   width: "100%",
-  flexGrow: "1"
+  flexGrow: "1",
+  overflowY: "scroll"
 });
 
 const ChatComponent = props => {
   const [message, setMessage] = useState();
+  const user = useContext(UserContext);
 
   const sendMessage = () => {
     if (String(message).trim()) {
-      console.log(message);
+      db.collection("rooms")
+        .doc(props.roomId)
+        .collection("chats")
+        .add({
+          name: user.displayName,
+          avater: user.photoURL,
+          message: message,
+          time: new Date()
+        });
     }
   };
+
   return (
     <Div>
-      <RoomHeader roomName="DuGlaser" />
+      <RoomHeader roomName={props.roomName} />
       <ChatArea>
         {props.loading && <p>loading</p>}
         {!props.loading &&
-          props.values.map(value => <MessageTile message={value.message} />)}
+          props.values.map(value => (
+            <MessageTile
+              key={value.id}
+              src={value.avater}
+              name={value.name}
+              message={value.message}
+            />
+          ))}
       </ChatArea>
       <SendForm setMessage={setMessage} sendMessage={sendMessage} />
     </Div>
